b484bec00a7591720d2c5a1425e0a19217f38730
omer <omertopak1@gmail.com>
2024-02-10 13:05:54 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+app.use(express.json()) //?gelen veriyi otomatik tur donusumu yapar
+
+
+/* ------------------------------------------------------- */
+//? >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>HATA YONETIMI >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
