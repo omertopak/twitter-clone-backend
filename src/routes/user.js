8c5f09c268e6ac60169a316a55ddc6d1ecf73b28
omer <omertopak1@gmail.com>
2024-03-03 17:15:31 +0000

    up

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ const router = require('express').Router()
 // Call Controllers:
 const { User } = require('../controllers/user')
 
-//PERMISSION LIST ADMIN
+
 router.route('/') //admin
     .get(User.list)
 router.post('/register', User.create) 
@@ -13,5 +13,8 @@ router.route('/:userId')
     .get(User.read) //herkes
     .put(User.update) //user
     .delete(User.delete) //user
+router.route('/:userId/follow')
+    .post(User.follow)
+
 
 module.exports = router
