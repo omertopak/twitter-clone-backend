e537715563c49b74fa7ae0cf653b9103f209c4de
omer <omertopak1@gmail.com>
2024-03-21 23:35:59 +0000

    up

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -7,7 +7,7 @@ module.exports = {
     create: async (req, res) => {
 
         const data = await Notification.create(req.body)
-        // const data = req.bod
+        // const data = req.body
        
 
         res.status(201).send({
