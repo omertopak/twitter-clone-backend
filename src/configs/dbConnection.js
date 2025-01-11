57a7edd86ac7e127144975897a62daa96a5b8881
omertopak <omertopak1@gmail.com>
2025-01-11 15:31:42 +0000

    up

--- src/configs/dbConnection.js
+++ src/configs/dbConnection.js
@@ -1,8 +1,4 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
-// MongoDB Connection:
 
 const mongoose = require('mongoose')
 
--- src/middlewares/multer.js
+++ src/middlewares/multer.js
@@ -1,28 +1,5 @@
-// const multer = require('multer');
-// const path = require('path');
-
-// // Multer storage configuration
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, path.join(__dirname, '../uploads'));  
-//   },
-//   filename: (req, file, cb) => {
-//     const uniqueName = `${Date.now()}-${file.originalname}`;
-//     cb(null, uniqueName);
-//   },
-// });
-
-// // Multer middleware
-// const upload = multer({ 
-//   storage: storage, 
-//   limits: {fileCount:4} 
-// });
-
-// module.exports = upload;
-
 const multer = require('multer');
-const fs = require('fs'); // fs modülünü içe aktar
+const fs = require('fs'); 
 const path = require('path');
 
 // Multer storage configuration
@@ -30,7 +7,7 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const uploadPath = path.join(__dirname, '../uploads');
     
-    // Klasör yoksa oluştur
+    
     if (!fs.existsSync(uploadPath)) {
       fs.mkdirSync(uploadPath, { recursive: true });
     }
@@ -42,7 +19,7 @@ const storage = multer.diskStorage({
   },
 });
 
-// Multer middleware
+
 const upload = multer({
   storage: storage,
   limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
